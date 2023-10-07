378f5a577aad47c2cc40efec6c10f5dc80925ff9
fabioedoardoluigialberto <fabioedoardoluigialberto@gmail.com>
2023-10-07 17:06:43 -0400

    cleaner code

--- spectate.py
+++ spectate.py
@@ -1,12 +1,13 @@
+import requests
 import click
 import pyautogui
-import urllib.request
 import json
 from time import sleep
 import sys
 
 SERVER_URL = "http://elevenlogcollector-env.js6z6tixhb.us-west-2.elasticbeanstalk.com/ElevenServerLiteSnapshot"
-import requests
+INTERVAL = 1  # slows down clicking around
+
 
 def retrieve_url(url):
     response = requests.get(url)
@@ -14,11 +15,6 @@ def retrieve_url(url):
         return response.text
     else:
         return None
-INTERVAL = 1  # in s
-neutralCamNr = 0
-homeCamNr = 8
-awayCamNr = 9
-firstMatch = True
 
 
 class Position:
@@ -31,24 +27,23 @@ class Position:
 
 
 mappings = {
-        # { width: 1920, height: 1080 },
-        'ACTIVATEWINDOW': Position(10, 10),
-        'HOME': Position(600, 900),
-        'USERPROFILE': Position(1521, 488),
-        # 'JOINROOM': Position(900, 430),
-        'JOINROOM': Position(1850, 470),
-        'HIDEMOUSE': Position(1920-10, 1080-10),
-        'EXITROOM': Position(1217, 974),
-    }
+    'HOME': Position(600, 900),
+    'JOINROOM': Position(1850, 470),
+    'EXITROOM': Position(1217, 974),
+}
 
-@click.command()
-@click.argument('test', type=bool)
-def _clickPos(x, y, test=False):
-    pyautogui.moveTo(x, y)
-    # pyautogui.click()
-    pyautogui.mouseDown()
-    sleep(0.05)
-    pyautogui.mouseUp()
+
+def before_and_after_click(func):
+    def wrapper(*args, **kwargs):
+        type("M")
+        sleep(INTERVAL)
+        type("0")
+        sleep(INTERVAL)
+        func(*args, **kwargs)
+        type("M")
+        return
+    return wrapper
+
 
 def clickButton(button, move_only=False):
     x, y = mappings[button].x, mappings[button].y
@@ -63,32 +58,25 @@ def clickButton(button, move_only=False):
         print(f"Moved to {x, y}")
         sleep(0.3)
 
-def type(str):
-    print(f"Pressed {str}")
-    pyautogui.write(str)
+
+@before_and_after_click
+def clickListOfButtons(list_of_buttons, move_only=False):
+    for button in list_of_buttons if isinstance(list_of_buttons, list) else [list_of_buttons]:
+        clickButton(button, move_only=move_only)
+        sleep(INTERVAL)
 
 
 def joinRoom(test):
-    type("M")
-    type("0")
-    # clickButton("ACTIVATEWINDOW", move_only=test)
-    clickButton("HOME", move_only=test)
-    sleep(1)
-    clickButton("USERPROFILE", move_only=test)
-    sleep(1)
-    clickButton("JOINROOM", move_only=test)
-    sleep(1)
-    clickButton("HIDEMOUSE", move_only=test)
-    type("M")
+    clickListOfButtons("HOME JOINROOM".split())
 
 
 def exitRoom(test):
-    type("M")
-    type("0")
-    clickButton("HOME", move_only=test)
-    sleep(1)
-    clickButton("EXITROOM", move_only=test)
-    type("M")
+    clickListOfButtons("HOME EXITROOM".split())
+
+
+def type(str):
+    print(f"Pressed {str}")
+    pyautogui.write(str)
 
 
 def retrieve_url(url):
@@ -98,11 +86,8 @@ def retrieve_url(url):
     else:
         return None
 
-# Usage
 
 def isInRoom(user):
-    # TODO: This can be done via OCR instead, to minimize server load
-    # OR....can also get it from the logs
     content = json.loads(retrieve_url(SERVER_URL))
     users = [x for x in content['UsersInRooms'] if x['UserName'] == user]
     if (len(users) > 0):
@@ -112,16 +97,20 @@ def isInRoom(user):
     sys.stdout.flush()
     return False
 
+
 def print_mouse():
     mouse_position = pyautogui.position()
-    print(f"Mouse position: {mouse_position[0]:04d}, {mouse_position[1]:04d}", end='')
+    print(
+        f"Mouse position: {mouse_position[0]:04d}, {mouse_position[1]:04d}", end='')
     sys.stdout.write('\r')
+
 
 @click.command()
 @click.option('--test', '-t', is_flag=True, default=False, help='Whether to avoid clicks')
-@click.option('--user', '-u', default='faffopong', help='Username')
+@click.option('--user', '-u', help='Username', required=True)
 def main(user, test):
-    print(test)
+
+    # it assumes that menu is off in the UI
 
     while True:
         print(f"Waiting until {user} is in a room...")
@@ -133,12 +122,13 @@ def main(user, test):
         print(f"User {user} is in a room!",  end='')
         print(f"Joining room.")
         joinRoom(test)
+
         while inRoom:
             inRoom = isInRoom(user)
             print_mouse()
             sleep(INTERVAL)
-        print(f"👻 User {user} is not in a room anymore.")
-        print(f"Leavining room.")
+        print(f"User {user} is no longer in a room.")
+        print(f"Leaving room.")
         exitRoom(test)
 
 
